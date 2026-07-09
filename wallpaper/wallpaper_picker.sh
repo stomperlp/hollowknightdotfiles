30973996133196e33ec49458b41036a7f6592650
stomperlp <linusbest2007@gmail.com>
2026-07-09 11:39:58 +0200

    dotfiles

---
+++ wallpaper/wallpaper_picker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#!/bin/bash
+IMGS="$HOME/wallpaper"
+
+killall awww-daemon 2>/dev/null || true
+awww-daemon &
+sleep 1 # give the daemon time to initialize
+
+while true; do
+  awww img "$(find "$IMGS" -name "*.png" | shuf -n 1)" --transition-duration 1
+  sleep 150
+done
